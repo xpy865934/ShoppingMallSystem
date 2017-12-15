242db4bfeccc74d078b6d68d62400cc4960fcc39
xpy <865934097@qq.com>
2017-12-15 21:58:45 +0800

    1、完成struts2框架的配置

---
+++ src/com/xpy/test/TestAction.java
@@ -0,0 +1,13 @@
+package com.xpy.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport{
+
+	private static final long serialVersionUID = 8541100649922195573L;
+
+	public String testAction() {
+		System.out.println("action∑√Œ ≥…π¶");
+		return NONE;
+	}
+}
